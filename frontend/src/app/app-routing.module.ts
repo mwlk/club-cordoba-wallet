d762c917049a438185fec1320c42f683932c544a
Mirko Wlk <mirkoivowlk@gmail.com>
2026-09-23 20:02:39 -0300

    feat: add Page Not Found component and routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'credentials', pathMatch: 'full' },
   {
     path: 'credentials',
     loadChildren: () => import('./features/credentials/credentials.module').then(m => m.CredentialsModule)
-  }
+  },
+  { path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
